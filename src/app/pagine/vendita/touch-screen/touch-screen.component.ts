46db68834c9f561d940b79555a60af87a6ea7a1d
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-09-03 16:03:58 +0200

    css migliorato

--- src/app/pagine/vendita/touch-screen/touch-screen.component.ts
+++ src/app/pagine/vendita/touch-screen/touch-screen.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-touch-screen',
   templateUrl: './touch-screen.component.html',
-  styleUrl: './touch-screen.component.css'
+  styleUrls: ['./touch-screen.component.css']
 })
-export class TouchScreenComponent {
+export class TouchScreenComponent implements OnInit {
+  currentTime: string = '';
+  currentDate: string = '';
 
+  ngOnInit(): void {
+    this.setCurrentDate();
+    this.setCurrentTime();
+  }
+
+  setCurrentDate(): void {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const day = now.getDate().toString().padStart(2, '0');
+    this.currentDate = `${year}-${month}-${day}`;
+  }
+
+  setCurrentTime(): void {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    this.currentTime = `${hours}:${minutes}`;
+  }
 }
